7498a0ac74ced4cd700e48a7989b847491124cb0
Cerato <151639583+CeratoCopernic@users.noreply.github.com>
2024-03-12 14:04:39 +0100

    Delete Setup directory

--- Setup/ES313 - WINDOWS.bat
+++
@@ -1,3 +0,0 @@
-echo "Starting Jupyter Lab and running updates...""
-
-%UserProfile%\AppData\Local\Julia-1.2.0\bin\julia.exe %~dp0\configES313.jl
--- Setup/start.jl
+++
@@ -1,8 +0,0 @@
-using Pkg
-# change pwd 
-cd(joinpath(dirname(@__FILE__),".."))
-# activate environment
-Pkg.activate(pwd())
-@info "Launching from $(pwd())"
-using Pluto
-Pluto.run()
